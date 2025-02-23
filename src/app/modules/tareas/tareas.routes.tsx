5ab1c9da525ce23ee5d6c5093ea82e540e8e5276
thefurer <51844780+thefurer@users.noreply.github.com>
2025-02-22 21:00:42 -0500

    fix path

--- src/app/modules/tareas/tareas.routes.tsx
+++ src/app/modules/tareas/tareas.routes.tsx
@@ -2,7 +2,6 @@ import { lazy, Suspense } from 'react';
 import { RouteObject } from 'react-router-dom';
 import TareasPendientesPage from './pages/pendientes/index';
 import TareasListaPage from './pages/lista/index';
-import TareasAsignar from './pages/asignar/tareaspage';
 
 
 const TareasRoutes: RouteObject[] = [
@@ -11,14 +10,6 @@ const TareasRoutes: RouteObject[] = [
         element: (
             <Suspense fallback={null}>
                 <TareasPendientesPage />
-            </Suspense>
-        ),
-    },
-    {
-        path: 'asignar',
-        element: (
-            <Suspense fallback={null}>
-                <TareasAsignar />
             </Suspense>
         ),
     },
